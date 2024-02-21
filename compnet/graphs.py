7358664b7c0fde40d9177720a4a49e03fcc52600
Shoichi Yip <yip.syip.shoichi98@gmail.com>
2024-02-21 18:13:27 +0100

    added giant component part

--- compnet/graphs.py
+++ compnet/graphs.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 class ConfigModelGraph:
   """
   Instances of graphs generated with the configuration model given a certain
@@ -19,8 +17,10 @@ class ConfigModelGraph:
     self.degree_sequence = degree_sequence
     self.allow_isolated = allow_isolated
     self.stubs = self.generate_stubs()
+    self.nodes = np.unique(self.stubs)
+    self.edges = None
     self.rng = np.random.default_rng()
-  
+
   def generate_stubs(self):
     """
     Generate list of stubs, which are the id of each n-th vertex repeated
@@ -68,7 +68,59 @@ class ConfigModelGraph:
         for e in trial_values: current_stubs.remove(e)
     if (success == 0):
       raise ValueError("Graph not found.")
-    return current_edges
+    self.edges = current_edges
+
+  def find_components(self):
+    if self.edges == None:
+      raise ValueError("A graph must first be generated.")
+    # transform the edge list in an edge array
+    edges_arr = np.array(self.edges)
+    # choose a random node
+    nonvisited = np.array(self.nodes).copy()
+    self.rng.shuffle(nonvisited)
+    nonvisited = list(nonvisited)
+    components = []
+    for i_c in range(100_000):
+      if len(nonvisited) == 0:
+        break
+      # for each component we initialize a list of visited nodes
+      visited_c = set()
+      nonvisited_c = [nonvisited[-1]]
+      success_c = 0
+      for i_nc in range(100_000):
+        if len(nonvisited_c) == 0:
+          success_c = 1
+          break
+        # visit the last nonvisited_c
+        current_node = nonvisited_c[-1]
+        # which edges contain the current node?
+        current_edges_mask = np.sum(edges_arr == current_node, axis=1)
+        # select only index of edges who contain the current node
+        current_edges_index = current_edges_mask.nonzero()[0]
+        # select only the edges who contain the current node
+        current_edges = edges_arr[current_edges_index]
+        # get only the neighbouring nodes by removing the current node
+        # to the 1D list of nodes
+        current_neighb = set(np.setdiff1d(current_edges, current_node))
+        current_new_neighb = current_neighb - visited_c
+        # remove the current node from the nonvisited and add it to visited
+        visited_c.add(nonvisited_c.pop(-1))
+        # now add the neighbours to the nonvisited
+        nonvisited_c += current_new_neighb
+      if success_c == 0:
+        continue
+      else:
+        for visited_node in visited_c: nonvisited.remove(visited_node)
+        components.append(visited_c)
+    return components
+
+  def find_giant_component(self):
+    if self.edges == None:
+      raise ValueError("A graph must first be generated.")
+    components = self.find_components()
+    component_sizes = [len(component) for component in components]
+    max_component_size = max(component_sizes)
+    return max_component_size
 
 class ConfigModelDegreeGraph(ConfigModelGraph):
   def __init__(self, N, degree_dict, allow_isolated=False,
