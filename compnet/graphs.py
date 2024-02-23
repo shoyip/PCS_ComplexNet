2f6119bfafd45ef94777da05ac710c17ad1f726e
Shoichi Yip <yip.syip.shoichi98@gmail.com>
2024-02-23 12:52:01 +0100

    added computation for qcore

--- compnet/graphs.py
+++ compnet/graphs.py
@@ -16,8 +16,7 @@ class ConfigModelGraph:
     self.N = N
     self.degree_sequence = degree_sequence
     self.allow_isolated = allow_isolated
-    self.stubs = self.generate_stubs()
-    self.nodes = np.unique(self.stubs)
+    self.nodes = np.arange(self.N)
     self.edges = None
     self.rng = np.random.default_rng()
 
@@ -44,7 +43,8 @@ class ConfigModelGraph:
     Iterate through the stubs two-by-two and create edges. Check that edges are
     neither multiple edges nor are self-edges.
     """
-    current_stubs = self.stubs
+    self.stubs = self.generate_stubs()
+    current_stubs = self.stubs.copy()
     current_edges = []
     success = 0
     for i in range(max_iter):
@@ -121,6 +121,39 @@ class ConfigModelGraph:
     component_sizes = [len(component) for component in components]
     max_component_size = max(component_sizes)
     return max_component_size
+
+  def find_neighbourhoods(self):
+    """
+    Given a graph return the dictionary of neighbours for each vertex.
+    Returns a dictionary of sets.
+    """
+    neighbourhoods = defaultdict(set)
+    for edge in self.edges:
+      neighbourhoods[edge[0]].add(edge[1])
+      neighbourhoods[edge[1]].add(edge[0])
+    self.neighbourhoods = dict(neighbourhoods)
+
+  def find_degrees(self, neighbourhoods):
+    """
+    Given a neighbourhood dictionary, compute the degree for each vertex.
+    """
+    degrees = {k: len(v) for k, v in neighbourhoods.items()}
+    return degrees
+
+  def get_biggest_qcoresize(self, q):
+    current_neighbourhoods = self.neighbourhoods.copy()
+    # fictitious values to initialize the iteration
+    current_qcoresize = 1
+    previous_qcoresize = 0
+    while (current_qcoresize > previous_qcoresize):
+      current_degrees = self.find_degrees(current_neighbourhoods)
+      previous_qcoresize = len(current_neighbourhoods)
+      current_neighbourhoods = {k: v
+      				for k, v
+      				in current_neighbourhoods.items()
+      				if current_degrees[k] >= q}
+      current_qcoresize = len(current_neighbourhoods)
+    return current_qcoresize
 
 class ConfigModelDegreeGraph(ConfigModelGraph):
   def __init__(self, N, degree_dict, allow_isolated=False,
