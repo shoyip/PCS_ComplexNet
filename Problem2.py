79f68a15da08818dcf6e2007acb5e71f682bb27c
Shoichi Yip <yip.syip.shoichi98@gmail.com>
2024-03-06 20:50:48 +0100

    completed problem 2

--- Problem2.py
+++ Problem2.py
@@ -13,13 +13,16 @@ b-c) We can generalize the analytical computation of the giant component size to
 the case of the configuration model with a given degree distribution. It is
 given by
 
-TODO
+   gamma = 1 - (1-pi) * (1-sqrt(pi)) / (2*sqrt(pi))
+                - pi * ( (1-sqrt(pi)) / (2*sqrt(pi)) )^4
 
 """
 
 from compnet.graphs import ConfigModelDegreeGraph
 import numpy as np
 import matplotlib.pyplot as plt
+
+# PART A: GENERATE INSTANCES AND MEASURE THE GIANT COMPONENT
 
 # we pick equally spaced values of pi from 0.01 to 0.99
 pi_values = np.linspace(0.01, 0.99, 15)
@@ -29,6 +32,7 @@ gcs_means, gcs_stds = [], []
 
 # we iterate over values of the pi parameter
 for pi in pi_values:
+    print(f'Generating data for instances with pi={pi}')
 
     # we define the number of vertices of a graph
     N = 1_000
@@ -61,9 +65,45 @@ for pi in pi_values:
     # compute the standard deviation of the giant component sizes
     gcs_stds.append(np.std(gcs_samples) / np.sqrt(20))
 
+# PART B: GENERATING THE PLOT FOR THE ANALYTICAL COMPUTATION OF THE
+# GIANT COMPONENT SIZE
+
+def gamma_func(pi):
+    """
+    This is the gamma function returning the probability that a vertex belongs
+    to the giant component in the specific case of p1=1-pi and p4=pi for a
+    configuration model random graph instance.
+
+    :param pi: input parameter pi
+    :returns gamma: the probability that a vertex belongs to the GC
+    """
+
+    def mu_func(pi):
+        """
+        This is the mu function returning the probability that an end vertex
+        does not belong to the GC in the specific case of p1=1-pi and p4=pi for
+        a configuration model random graph instance.
+
+        :param pi: input parameter pi
+        :returns gamma: the probability that an end vertex does not belong to
+        the GC
+        """
+        return (1-np.sqrt(pi)) / (2*np.sqrt(pi))
+
+    # we put a threshold where we know that the GC will appear
+    if pi > 1. / 9.:
+        return 1 - (1 - pi) * mu_func(pi) - pi * (mu_func(pi))**4
+    else:
+        return 0.
+
+analytic_pi = np.linspace(0, 1, 200)
+analytic_gamma = np.vectorize(gamma_func)(analytic_pi)
+
+# generate the figure
 fig = plt.figure()
 plt.errorbar(pi_values, gcs_means, yerr=gcs_stds, fmt='.',
     label='GCS from random samples')
+plt.plot(analytic_pi, analytic_gamma, label=r'$\gamma$ function')
 plt.grid()
 plt.legend()
 plt.title('Behaviour of Giant Component Size in the configuration model')
@@ -71,4 +111,5 @@ plt.xlabel(r'$\pi$')
 plt.ylabel(r'Giant Component Size')
 plt.close()
 
+# saving the figure
 fig.savefig('assets/gcs.pdf', bbox_inches='tight')
