2ee6d269c5d33bedc2e30073d56db0a62359a7d6
Shoichi Yip <yip.syip.shoichi98@gmail.com>
2024-03-25 09:47:13 +0100

    nearly completed slides for first three parts

--- Problem3.py
+++ Problem3.py
@@ -55,17 +55,51 @@ def get_threecoresize(pi):
     )
     return threecoresize / n_vertices
 
+# generate example ode figure
+
+pi = .3
+_, U = get_theo_qcore_size(
+        n_vertices = 1_000,
+        degree_distribution = [0, 1-pi, 0, 0, pi],
+        q = 3,
+        function = threecore_step)
+
+fig = plt.figure()
+for i in range(5):
+    plt.plot(U[:-1, i], label=f'$d={i}$')
+plt.title('Nodes per degree in time for stochastic 3-core for $pi=0.3$')
+plt.xlabel(r'$T$')
+plt.ylabel(r'$p_d(t)$')
+plt.grid()
+plt.legend()
+plt.close()
+
+fig.savefig('assets/ode_evol_pi03.pdf', bbox_inches='tight')
+
+pi = .8
+_, U = get_theo_qcore_size(
+        n_vertices = 1_000,
+        degree_distribution = [0, 1-pi, 0, 0, pi],
+        q = 3,
+        function = threecore_step)
+
+fig = plt.figure()
+for i in range(5):
+    plt.plot(U[:-1, i], label=f'$d={i}$')
+plt.title('Nodes per degree in time for stochastic 3-core for $pi=0.8$')
+plt.xlabel(r'$T$')
+plt.ylabel(r'$p_d(t)$')
+plt.grid()
+plt.legend()
+plt.close()
+
+fig.savefig('assets/ode_evol_pi08.pdf', bbox_inches='tight')
+
 # computing theoretical values
 
 print("Computing theoretical q-core sizes...")
 
 get_threecoresize_vect = np.vectorize(get_threecoresize)
-
-# theo_qcoresize, _ = get_theo_qcore_size(
-#         n_vertices = 10_000,
-#         degree_distribution=[0, 1-pi, 0, 0, pi],
-#         q=3,
-#         function=threecore_step)
 
 theo_pi_values = np.linspace(0.1, 0.99, 50)
 
@@ -89,4 +123,4 @@ fig.savefig('assets/qcore.pdf', bbox_inches='tight')
 
 stop = time.time()
 
-print(f'Time elapsed: {(stop-start) / 60} mins')
+print(f'Time elapsed: {(stop-start)/60:.0f}m{(stop-start)%60:.0f}s')
